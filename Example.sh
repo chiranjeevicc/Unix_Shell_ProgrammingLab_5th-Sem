2f72f5722c71f286cd7655c21135d874215f6c78
chiranjeevi <56463677+chiranjeevicc@users.noreply.github.com>
2020-10-01 13:59:45 +0530

    Create Example.sh

---
+++ Example.sh
@@ -0,0 +1,7 @@
+
+#! /bin/sh
+#above is required for bash
+echo "Today's date: `date`"  # enclose unix cmds in backquotes ` not ' 
+echo "My Shell : $SHELL"  
+echo "This Months's Calender: `cal`"
+echo "Date: `date "+%d:%m:%Y"`"  # after date use a space and a plus sign
