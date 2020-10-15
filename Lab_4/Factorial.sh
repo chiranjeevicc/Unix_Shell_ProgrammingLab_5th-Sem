af9d7ff99f5bf7efecff431f6dcf9bcc6f4a1db8
chiranjeevi <56463677+chiranjeevicc@users.noreply.github.com>
2020-10-15 14:35:48 +0530

    Create Factorial.sh

---
+++ Lab_4/Factorial.sh
@@ -0,0 +1,15 @@
+#1.	Shell script to  find the FACTORIAL OF A NUMBER using 
+# Looping construct
+
+echo "Enter a number"
+read num
+
+fact=1
+
+while [ $num -gt 1 ]
+do
+  fact=$((fact * num))  #fact = fact * num
+  num=$((num - 1))      #num = num - 1
+done
+
+echo $fact
