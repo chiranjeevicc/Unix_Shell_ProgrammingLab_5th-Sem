b66c877c6070cecfd87303d6f3a107e85f0ba535
chiranjeevi <56463677+chiranjeevicc@users.noreply.github.com>
2020-11-09 13:24:30 +0530

    Delete Word_ count.sh

--- Word_ count.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-echo "Enter the file path:"
-read path
-w=`cat $path | wc -w`
-l=`cat $path | wc -l`
-c=`cat $path | wc -c`
-output="Words: $w | Lines: $l | Characters: $c"
-echo $output
